a555c8fe83c779b1f6c81b1ffc2223245b36434f
¨pepocasella¨ <pepocasella@gmail.com>
2025-06-01 19:11:54 -0300

    adicionando ação Editar + refatoração Async Await

--- CRUD/README.md
+++ CRUD/README.md
@@ -2,6 +2,9 @@ abrir a pasta admin no vscode e
 rodar o json server: 
 ```js
 json-server --watch db.json
+npx json-server --watch db.json
 ```
 
 Rodar: browser-sync start --server --file . --host --port 5000 --startPath admin/telas/lista_cliente.html
+
+
--- CRUD/service/cliente-service.js
+++ CRUD/service/cliente-service.js
@@ -1,9 +1,10 @@
-// Conexão com API MOCK
-
-const listaClientes = () => {
-    return fetch('http://localhost:3000/profile')
-    .then( resposta => {
-        return resposta.json()
+const listaClientes = () =>  {
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
@@ -19,19 +20,59 @@ const criaCliente = (nome, email) => {
         })
     })
     .then( resposta => {
-        return resposta.body
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar um cliente')
     })
 }
 
-const deletaCliente = (id) => {
+const removeCliente = (id) => { 
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
     })
+    .then( resposta => { 
+        if(!resposta.ok){
+        throw new Error('Não foi possível deletar um cliente')
+        }
+    })
+}
+ 
+const detalhaCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => { 
+        if(resposta.ok){
+            return resposta.json()
+        }
+    
+        throw new Error('Não foi possível detalhar um cliente')
+    })
 }
 
-export const clienteService = {
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: { 
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome, 
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar um cliente')
+    })
+}
+
+export const clienteService = { 
     listaClientes,
-    criaCliente,
-    deletaCliente
-}
+    criaCliente, 
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 
+}
